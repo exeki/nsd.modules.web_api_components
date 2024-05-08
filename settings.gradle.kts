649c1299f85c5d2d46a628079a336a15d3373812
ekazantsev <ekazantsev@ocs.ru>
2024-05-08 19:45:04 +0400

    чето починил, попробовал добавить автосборку с публикацией на гитхаб

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,14 @@
 rootProject.name = "web_api_components"
 
+pluginManagement {
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/exeki/*")
+            credentials {
+                username = System.getenv("GITHUB_USERNAME")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+        mavenCentral()
+    }
+}
